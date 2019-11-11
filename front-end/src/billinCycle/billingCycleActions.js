9432aba8654452a37fde0478dc3de0974240d6a1
E.STECH <e.s.tech>
2019-11-10 22:49:01 -0300

    Exibindo as mensagens de Sucesso e de Erro

--- front-end/src/billinCycle/billingCycleActions.js
+++ front-end/src/billinCycle/billingCycleActions.js
@@ -1,3 +1,4 @@
+import { toastr } from 'react-redux-toastr';
 import axios from 'axios';
 const BASE_URL = 'http://localhost:3003/api'
 
@@ -11,6 +12,12 @@ export function getList() {
 
 export function create(values) {
     axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+        })
+        .catch(e => {
+            e.response.data.errors.forEach(error => toastr.error('Erro', error))
+        })
     return {
         type: 'TEMP'
     }
